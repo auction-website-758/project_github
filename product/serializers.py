79d6339023e292fc002f78abd4b5840e7148e689
auction-website-758 <auctionwebsite000@gmail.com>
2022-12-15 08:10:59 +0500

    comm

---
+++ product/serializers.py
@@ -0,0 +1,64 @@
+from rest_framework import serializers
+
+from .models import Product,Auction,Question,Answers, Profile
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            "id",
+            "name",
+            "get_absolute_url",
+            "description",
+            "price",
+            "end_date",
+            "bid_active",
+            "get_image"
+        )
+
+class CreateAuctionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            "id",
+            "name",
+            "slug",
+            "description",
+            "price",
+            "end_date"
+        )
+
+class AuctionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Auction
+        fields = (
+            "id",
+            "bid"
+        )
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = (
+            "id",
+            "question_text"
+        )
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answers
+        fields = (
+            "id",
+            "answer"
+        )
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = (
+            "id",
+            "email",
+            "date_of_birth"
+        )
+    
